e815c8cf187a14ad375793871b8a6e8519f643b5
Artur Bańkowski <artur.bankowski@gmail.com>
2021-07-26 17:14:13 +0200

    Wire upgrade commands

--- cmd/updater/commands/selfupgrade.go
+++ cmd/updater/commands/selfupgrade.go
@@ -6,24 +6,23 @@ import (
 )
 
 func init() {
-
-	rootCmd.AddCommand(executeUpgradeCmd)
-	executeUpgradeCmd.Flags().StringP("version", "v", "latest", "target version")
+	rootCmd.AddCommand(executeSelfUpgradeCmd)
+	executeSelfUpgradeCmd.Flags().StringP("version", "v", "latest", "target version")
 	executeSelfUpgradeCmd.Flags().BoolP("force", "f", false, "force upgrade")
 }
 
-func executeUpgrade(cmd *cobra.Command, args []string) {
+func executeSelfUpgrade(cmd *cobra.Command, args []string) {
 	version, _ := cmd.Flags().GetString("version")
 	// TODO: if version is latest then handle it
 
 	// TODO: check if current version is matching our target; abandon if not forced
 
-	updater.CommandlineUpgrade(version).Run()
+	updater.SelfUpgrade(version).Run()
 }
 
-var executeUpgradeCmd = &cobra.Command{
-	Use:   "upgrade",
-	Short: "Upgrade for your Constellation Labs commands tools",
+var executeSelfUpgradeCmd = &cobra.Command{
+	Use:   "self-upgrade",
+	Short: "Selfupgrade of upgrade manager",
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 	},
